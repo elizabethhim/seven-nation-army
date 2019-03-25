eb5069e2d0079d85caea3f5181a6343552409323
BornIncompetence <cagbrothers@gmail.com>
2019-03-25 01:36:57 -0700

    Welcome Screen now renders first
    However, I forgot to ask the person who provided the html for the images he used.

--- Seven-Nation-Army/app/Routes.js
+++ Seven-Nation-Army/app/Routes.js
@@ -4,12 +4,14 @@ import routes from './constants/routes';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
+import WelcomePage from './containers/WelcomePage';
 
 export default () => (
   <App>
     <Switch>
       <Route path={routes.COUNTER} component={CounterPage} />
       <Route path={routes.HOME} component={HomePage} />
+      <Route path={routes.WELCOME} component={WelcomePage} />
     </Switch>
   </App>
 );
---
+++ Seven-Nation-Army/app/components/Welcome.js
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React, { Component } from 'react';
+import './Welcome.css';
+
+export default class Welcome extends Component {
+  render() {
+    return (
+      <div className="container">
+        <img
+          className="resize_fit_center background"
+          src="images/main_menu_bg_w_title.png"
+        />
+        <img
+          className="item resize_fit_center screen_item"
+          src="images/main_menu_new_game.png"
+        />
+        <img
+          className="item resize_fit_center screen_item"
+          style={{ top: '30%' }}
+          src="images/main_menu_continue.png"
+        />
+        <img
+          className="item resize_fit_center screen_item"
+          style={{ top: '60%' }}
+          src="images/main_menu_settings.png"
+        />
+      </div>
+    );
+  }
+}
