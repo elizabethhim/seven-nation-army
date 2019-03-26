0c49a47d2b7675e8c012e0273e96719ac8d1c6d1
Mercer Denholm <mercer233@gmail.com>
2019-03-26 16:16:47 -0700

    added the new bp

---
+++ Seven-Nation-Army2/src/app.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <p>helege</p>
+        <h1>feroiijofije</h1>
+        <Button color="danger">Danger!</Button>
+
+ 
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Seven-Nation-Army2/webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+              }
+        ]
+    }
+};
+module.exports = config;
