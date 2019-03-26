d68c4f98c378d4c34fc729b0556369529dab1351
Enrique Zuniga <36870131+zuniganoel@users.noreply.github.com>
2019-03-26 13:28:59 -0700

    fixed welcome

--- Seven-Nation-Army/app/components/Welcome.js
+++ Seven-Nation-Army/app/components/Welcome.js
@@ -2,38 +2,39 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Button } from 'reactstrap';
+import styles from './Welcome.css';
 
 export default class Welcome extends Component {
   render() {
     return (
-      // <div className="container">
-      //   <img
-      //     className="resize_fit_center background"
-      //     src="images/main_menu_bg_w_title.png"
-      //   />
-      //   <img
-      //     className="item resize_fit_center screen_item"
-      //     src="images/main_menu_new_game.png"
-      //   />
-      //   <img
-      //     className="item resize_fit_center screen_item"
-      //     style={{ top: '30%' }}
-      //     src="images/main_menu_continue.png"
-      //   />
-      //   <img
-      //     className="item resize_fit_center screen_item"
-      //     style={{ top: '60%' }}
-      //     src="images/main_menu_settings.png"
-      //   />
-      // </div>
-      <div>
-        <NavLink to="/game" exact>
-          <Button color="primary">New Game</Button>
-          <Button color="primary">Continue</Button>
-          <Button color="primary">Settings</Button>
-          <Button color="primary">Exit</Button>
-        </NavLink>
+      <div className="container">
+        <img
+          className="background resize_fit_center"
+          src="../resources/images/main_menu_bg_w_title.png"
+        />
+        <img
+          className="screen_item resize_fit_center item"
+          src="../resources/images/main_menu_new_game.png"
+        />
+        <img
+          className="screen_item resize_fit_center item"
+          style={{ top: '30%' }}
+          src="../resources/images/main_menu_continue.png"
+        />
+        <img
+          className="screen_item resize_fit_center item"
+          style={{ top: '60%' }}
+          src="../resources/images/main_menu_settings.png"
+        />
       </div>
+      // <div>
+      //   <NavLink to="/game" exact>
+      //     <Button color="primary">New Game</Button>
+      //     <Button color="primary">Continue</Button>
+      //     <Button color="primary">Settings</Button>
+      //     <Button color="primary">Exit</Button>
+      //   </NavLink>
+      // </div>
     );
   }
 }
