f12972772665ccf082807066304fa527e9a0d3d4
Samantha Wiley <samantha.wiley@student.csulb.edu>
2019-03-28 13:10:36 -0700

    Update Chat.js

--- Seven-Nation-Army3/src/chat/components/ChatList.js
+++ Seven-Nation-Army3/src/chat/components/ChatList.js
@@ -6,39 +6,39 @@ export default class ChatList extends Component {
     return (
       <Fragment>
         <Person
-          name="Italy"
+          name="Vincent Port"
           isOnline
-          status="online"
+          status="Italy"
           source="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_01.jpg"
         />
         <Person
           name="Aiden Chavez"
           isOnline={false}
-          status="left 7 minutes ago"
+          status="Germany"
           source="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_02.jpg"
         />
         <Person
           name="Mike Thomas"
           isOnline
-          status="online"
+          status="France"
           source="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_03.jpg"
         />
         <Person
           name="Erica Hughes"
           isOnline
-          status="online"
+          status="Russia"
           source="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_04.jpg"
         />
         <Person
           name="Ginger John"
           isOnline
-          status="online"
+          status="Turkey"
           source="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_05.jpg"
         />
         <Person
           name="Tracy Carp"
           isOnline={false}
-          status="left 30 mins ago"
+          status="Austria"
           source="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_06.jpg"
         />
       </Fragment>
