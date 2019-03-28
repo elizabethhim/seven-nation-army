ed81ae5d053a89018fd6160a4f3513519980db69
BornIncompetence <christopher_greer@elcamino.edu>
2019-03-28 13:48:15 -0700

    Fixed Chat CSS for real this time

--- Seven-Nation-Army3/src/game/map/BackButton.js
+++ Seven-Nation-Army3/src/game/map/BackButton.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { Button } from 'reactstrap';
+import '../../styles/BackButton.scss';
 
 export default class BackButton extends Component {
   render() {
     return (
-      <Button>
-        <Link to="/">
+      <NavLink to="/">
+        <Button className="back-button">
           <FontAwesomeIcon icon={faArrowLeft} />
-        </Link>
-      </Button>
+        </Button>
+      </NavLink >
     )
   }
 }
