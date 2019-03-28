b7f09d809e8d82bae2588a1a11ea192bbd370ce2
Ponce-1 <37884033+Ponce-1@users.noreply.github.com>
2019-03-27 21:48:12 -0700

    route fix

--- Seven-Nation-Army3/src/routes.js
+++ Seven-Nation-Army3/src/routes.js
@@ -9,9 +9,9 @@ import About from './about';
 import Login from './login/login';
 import Map from './game/map/map';
 import Settings from './settings/settings';
-import Help from './help/help'
+import Help from './help/help';
 import PageNotFound from './common/components/PageNotFound';
-import { settings } from 'cluster';
+
 
 export default (
   <Router>
@@ -24,7 +24,7 @@ export default (
       <Route exact path="/" component={Home}/>
       <Route path="/map" component={Map}/>
       <Route path="/settings" component={Settings}/>
-      <Route path="/help" component={help}/>
+      <Route path="/help" component={Help}/>
       <Route path="/about" component={About}/>
       <Route path="/login" component={Login}/>
       <Route component={PageNotFound} />
