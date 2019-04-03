44d5f01ad5a71aac61c347bd1e76206b7688d877
Enrique Zuniga <36870131+zuniganoel@users.noreply.github.com>
2019-04-02 22:47:45 -0700

    Organized things better
    Arrays containing information were moved to the constructor.
    Init function lays out the starting positions, adds 'unit' and 'country' attributes to the territories.
    highlight() is now called, addListeners() and is only responsible for adding listeners to the territories.

--- Seven-Nation-Army3/src/game/map/map.js
+++ Seven-Nation-Army3/src/game/map/map.js
@@ -15,6 +15,7 @@ export default class Map extends Component {
 
 	constructor(props) {
 		super(props);
+		
 		this.starting = [
 				  [["Vienna", 0], ["Budapest", 0], ["Trieste", 1]],
 				  [["Edinburgh", 1], ["Liverpool", 0], ["London", 1]],
@@ -24,45 +25,10 @@ export default class Map extends Component {
 				  [["St_Petersburg", 1], ["Moscow", 0], ["Warsaw", 0], ["Sevastopol", 1]],
 				  [["Constantinople", 0], ["Ankara", 1], ["Smyrna", 0]]
 				 ];
-		this.init = this.init.bind(this);
-	}
 
-	init(){
-		const countries = ["Austria_Hungary", "England", "France", "Germany", "Italy", "Russia", "Turkey"];
-		const colors = ["#ed497d", "#605aa7", "#9a9148", "#c0495e", "#cb75db", "#c95df6", "#7b69b8"];
-		// 2D array
-		// const starting = [
-		// 				  [["Vienna", 0], ["Budapest", 0], ["Trieste", 1]],
-		// 				  [["Edinburgh", 1], ["Liverpool", 0], ["London", 1]],
-		// 				  [["Brest", 1], ["Paris", 0], ["Marseilles", 0]],
-		// 				  [["Kiel", 1], ["Berlin", 0], ["Munich", 0]],
-		// 				  [["Venice", 0], ["Rome", 0], ["Naples", 1]],
-		// 				  [["St_Petersburg", 1], ["Moscow", 0], ["Warsaw", 0], ["Sevastopol", 1]],
-		// 				  [["Constantinople", 0], ["Ankara", 1], ["Smyrna", 0]]
-		// 				 ];
-		const highlight_layer = document.getElementById('map_overlay');
-		if(highlight_layer){
-			const svgDoc = highlight_layer.contentDocument;
-			for (let i = 0; i < this.starting.length; i++){
-				for (let j = 0; j < this.starting[i].length; j++){
-					const value = svgDoc.getElementById(this.starting[i][j][0]);
-					if(value){
-						value.setAttribute('stroke', colors[i]);
-						value.setAttribute('stroke-width', 8);
-						value.setAttribute('stroke-opacity', .5);
-					}
-				}
-			}
-		}
-		this.highlight();
-	}
-
-	highlight() {
-		const countries = ["Austria_Hungary", "England", "France", "Germany", "Italy", "Russia", "Turkey"];
-		const colors = ["#ed497d", "#605aa7", "#9a9148", "#c0495e", "#cb75db", "#c95df6", "#7b69b8"];
-
-		// Array of all the territory names, used as reference
-		const names = [
+		this.countries = ["Austria_Hungary", "England", "France", "Germany", "Italy", "Russia", "Turkey"];
+		this.colors = ["#ed497d", "#605aa7", "#9a9148", "#c0495e", "#cb75db", "#c95df6", "#7b69b8"];
+		this.territoryNames = [
 			'Switzerland',
 			'Adriatic_Sea',
 			'Aegean_Sea',
@@ -141,60 +107,77 @@ export default class Map extends Component {
 			'Yorkshire'
 		];
 
-		const highlight_layer = document.getElementById('map_overlay');
-		let territories = [];
+		this.territoryObjects = [];
 
-		// Opens the map_overlay svg document and iterates over all paths within it
-		// Each path will be stored in the territories array
-		// Then the we iterate over the territories array and attach an EventListener to each item in the array
-		// These eventListeners will look for mouseover and mouseout events and modify the path accordingly
-		// Currently it is set to change the color and opacity on enter and exit
-		if (highlight_layer) {
+		this.init = this.init.bind(this);
+		this.mouseMove = this.mouseMove.bind(this);
+	}
 
-			const svgDoc = highlight_layer.contentDocument;
-			for (let i = 0; i < 76; i++) {
-				const value = svgDoc.getElementById(names[i]);
-				if (value) {
-					value.style.fill = "yellow";
-					value.addEventListener(
-						'mouseenter',
-						() => {
-							value.setAttribute("fill-opacity", 0.33);
-							const territoryOwner = typeof(countries[colors.indexOf(value.getAttribute('stroke'))]) != 'undefined' ? countries[colors.indexOf(value.getAttribute('stroke'))] : 'Unoccupied';
-							document.getElementById('myPopup').classList.toggle('show');
-							const arr = this.starting[countries.indexOf(countries[colors.indexOf(value.getAttribute('stroke'))])];
-							let unit = "None";
-							if(typeof arr != "undefined"){
-								for(let i = 0; i < arr.length; i++){
-									if(value.id == arr[i][0]){
-										unit = arr[i][1] == 0 ? "Army" : "Fleet";
-										break;
-									}
-								}
-							}
-	
-							document.getElementById("popupText").innerHTML = "Country: " + territoryOwner + 
-																			"<br/> Territory: " + value.id + 
-																			"<br/> Unit: " + unit + 
-																			"<br/>" + "Player:  None";
-						}, false);
-					value.addEventListener(
-						'mouseout',
-						() => {
-							value.setAttribute("fill-opacity", 0);
-							if (document.getElementsByClassName('show').length != 0){
-								document.getElementById('myPopup').classList.toggle('show');
-							}
-						}, false);
-					value.addEventListener(
-						'click',
-						() => {
-							console.log("CLICK");
-						}, false);
+	mouseMove(){
+
+	}
+
+	init(){
+		const svgLayer = document.getElementById('map_overlay');
+		if(svgLayer){
+			const svgDoc = svgLayer.contentDocument;
+			for(let i = 0; i < this.territoryNames.length; i++){
+				const territory = svgDoc.getElementById(this.territoryNames[i]);
+				if(territory){
+					territory.setAttribute('country', 'Unclaimed');
+					territory.setAttribute('unit', 'None');
+
+					this.territoryObjects.push(territory);
 				}
-
+			}
+			for (let i = 0; i < this.starting.length; i++){
+				for (let j = 0; j < this.starting[i].length; j++){
+					const territory = svgDoc.getElementById(this.starting[i][j][0]);
+					if(territory){
+						territory.setAttribute('stroke', this.colors[i]);
+						territory.setAttribute('stroke-width', 8);
+						territory.setAttribute('stroke-opacity', .5);
+						territory.setAttribute('country', this.countries[i]);
+						let unit;
+						switch(this.starting[i][j][1]){
+							case 0:
+								unit = 'Army';
+								break;
+							case 1:
+								unit = 'Fleet';
+								break;
+						}
+						territory.setAttribute('unit', unit);
+					}
+				}
 			}
 		}
+		this.addListeners();
+	}
+
+	addListeners() {
+	    for (let i in this.territoryObjects) {
+	    	let territory = this.territoryObjects[i];
+	    	if(territory){
+		      	territory.style.fill = "yellow";
+		      	territory.addEventListener('mouseenter', () => {
+					territory.setAttribute("fill-opacity", 0.33);
+
+					document.getElementById('myPopup').classList.toggle('show');
+					document.getElementById("popupText").innerHTML = "Country: " + territory.getAttribute('country') + 
+																	"<br/> Territory: " + territory.id + 
+																	"<br/> Unit: " + territory.getAttribute('unit') + 
+																	"<br/>" + "Player:  None";
+				});
+		    	
+		    	territory.addEventListener('mouseout', () => {
+					territory.setAttribute("fill-opacity", 0);
+					if (document.getElementsByClassName('show').length != 0){
+						document.getElementById('myPopup').classList.toggle('show');
+					}
+				});
+	    	}
+	    }
 	}
 
 	render() {
