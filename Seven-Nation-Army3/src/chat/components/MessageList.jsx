496aad675ac5657c3ffb5d7a01abd7ee6a4bb7f3
Samantha Wiley <samantha.wiley@student.csulb.edu>
2019-05-08 16:56:46 -0700

    Listen to messages on firebase

--- Seven-Nation-Army3/src/chat/components/MessageList.jsx
+++ Seven-Nation-Army3/src/chat/components/MessageList.jsx
@@ -3,27 +3,54 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import '../../styles/Chat.scss';
 import ChatMessage from './ChatMessage';
+import { getFirebase } from 'react-redux-firebase';
 
-// const ConnectedList = ({ messages }) => {
-//   return (
-//     messages &&
-//     messages.map(el => (
-//       <ChatMessage
-//         key={el.messageID}
-//         myMessage={true}
-//         time={el.time}
-//         name={el.senderName}
-//         message={el.message}
-//       />
-//     ))
-//   );
-// };
+export default class MessageList extends Component {
 
-class ConnectedList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: []
+    };
+    this.firebase = getFirebase();
+    this.chatroomListener = null;
+    this.chatroomRef = null;
+    this.listenForNewMessages = this.listenForNewMessages.bind(this);
+  }
+
+  listenForNewMessages(props) {
+    this.chatroomRef = this.firebase.database().ref('root/chatrooms/' + props.roomID);
+    this.chatroomListener = this.chatroomRef.on('value', snapshot => {
+      const messageList = [];
+      snapshot.forEach(element => {
+        messageList.push({
+          senderID: element.val().senderID,
+          senderName: element.val().senderName,
+          time: element.val().time,
+          message: element.val().message,
+          messageID: element.key,
+        });
+      });
+      this.setState({
+        messages: messageList,
+      });
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.roomID) {
+      if(this.messageRef) {
+        this.messageRef.off("value", this.chatroomListener);
+      }
+      this.listenForNewMessages(nextProps);
+    }
+  }
+
   render() {
+    // console.log(this.state.messages);
     return (
-      this.props.messages &&
-      this.props.messages.map(el => (
+      this.state.messages &&
+      this.state.messages.map(el => (
         <ChatMessage
           key={el.messageID}
           myMessage={true}
@@ -36,16 +63,9 @@ class ConnectedList extends Component {
   }
 }
 
-ConnectedList.propTypes = {
-  messages: PropTypes.any,
+MessageList.propTypes = {
   roomID: PropTypes.any,
   friendID: PropTypes.any,
   friendUsername: PropTypes.any,
 };
 
-const mapStateToProps = state => ({
-  // console.log(state);
-  messages: state.chatHistory.messages
-});
-
-export default connect(mapStateToProps)(ConnectedList);
